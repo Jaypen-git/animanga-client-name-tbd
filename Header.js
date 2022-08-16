1dd258adf833f73dd01326b4adec42051a3767f8
Jeremy Penaredondo <jeremypenaredondo97@gmail.com>
2022-08-15 21:17:22 -0400

    adds basic header

--- Header.js
+++ Header.js
@@ -1,7 +1,9 @@
 const Header = () => {
     return (
         <header>
-            <h1>Animanga Client</h1>
+            <h1 className="title">Animanga Client</h1>
+            <input type="text" name="search" id="search" />
+            <button>Search</button>
         </header>
     );
 };
